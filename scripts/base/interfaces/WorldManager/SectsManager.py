280bf4d9f2579ed492b66ce74719e84982343d38
m969 <969041327@qq.com>
2018-02-06 20:33:58 +0800

    up

--- scripts/base/interfaces/WorldManager/SectsManager.py
+++ scripts/base/interfaces/WorldManager/SectsManager.py
@@ -12,30 +12,27 @@ class SectsManager:
     def __init__(self):
         DEBUG_MSG("SectsManager:__init__")
         KBEngine.globalData["SectsManager"] = self
-        # for (sectID, sectDBID) in self.sectDBIDList.items():
-        #     pass
         if not hasattr(self, "sectDBIDList"):
             self.sectDBIDList = {}
         self.sectList = {}
-        self.sectCounter = 0
-        # for sectID in self.sectDBIDList.keys():
-        #     self.sectList[sectID] = True
         for (sectID, sectData) in sect_data.data.items():
             if sectID in self.sectDBIDList.keys():
                 KBEngine.createBaseFromDBID("Sect", self.sectDBIDList[sectID], self.__sectCreateCallback)
             else:
                 sect = KBEngine.createBaseLocally("Sect", {"sectName": sectData["sectName"], "sectID": sectID})
-                sect.writeToDB(self.__onSectSaved)
+                sect.writeToDB(self.__onSectSaved, True)
 
 
     def __onSectSaved(self, success, sect):
         DEBUG_MSG("SectsManager:__onSectSaved")
         self.sectDBIDList[sect.sectID] = sect.databaseID
+        self.writeToDB(self.__onSectsManagerSaved, True)
 
 
     def __sectCreateCallback(self, baseRef, dbid, wasActive):
         DEBUG_MSG("SectsManager:__sectCreateCallback")
-        self.sectCounter = self.sectCounter + 1
         self.sectList[baseRef.sectID] = baseRef
-        if self.sectCounter >= len(self.sectDBIDList):
-            pass
+
+
+    def __onSectsManagerSaved(self, success, sectsManager):
+        DEBUG_MSG("SectsManager:__onSectsManagerSaved")
