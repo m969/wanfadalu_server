602af283c35f9baedd1f26d85c4845adb163fa0a
m969 <969041327@qq.com>
2018-02-11 00:08:18 +0800

    up

--- scripts/base/interfaces/Avatar/SectSystem.py
+++ scripts/base/interfaces/Avatar/SectSystem.py
@@ -8,25 +8,26 @@ from KBEDebug import *
 class SectSystem:
     def __init__(self):
         DEBUG_MSG("SectSystem:__init__")
-        if not hasattr(self, "sectData"):
-            self.sectData = { "sectID": self.sectID }
+        if self.sectID_b != 0:
+            if not hasattr(self, "sectData"):
+                self.sectData = { "sectID": self.sectID_b }
 
 
     def requestJoinSect(self, sectID):
         DEBUG_MSG("SectSystem:requestJoinSect")
-        if self.sectID != 0:
+        if self.sectID_b != 0:
             return
         KBEngine.globalData["sect_%i" % sectID].requestJoinSect(self, self.databaseID)
 
 
     def onJoinSectResult(self, sectID, result):
         DEBUG_MSG("SectSystem:onJoinSectResult")
-        self.sectID = sectID
+        self.sectID_b = sectID
         self.client.onJoinSectResult(sectID, result)
 
 
     def requestSelfSectData(self):
         DEBUG_MSG("SectSystem:requestSelfSectData")
-        if self.sectID == 0:
+        if self.sectID_b == 0:
             return
         self.client.onRequestSelfSectDataReturn(self.sectData)
