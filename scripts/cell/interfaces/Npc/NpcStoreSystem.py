afd21b521d2c3733b291bdf304706a0160ea6610
m969 <969041327@qq.com>
2018-02-28 18:32:42 +0800

    up

--- scripts/cell/interfaces/Npc/NpcStoreSystem.py
+++ scripts/cell/interfaces/Npc/NpcStoreSystem.py
@@ -8,3 +8,8 @@ from KBEDebug import *
 class NpcStoreSystem:
     def __init__(self):
         DEBUG_MSG("NpcStoreSystem:__init__")
+
+
+    def requestBuyProp(self, avatarBaseCall, propID):
+        DEBUG_MSG("NpcStoreSystem:requestBuyProp")
+        pass
