fde6bae2d8a9b6b560212231590ec9d21684df99
m969 <969041327@qq.com>
2018-03-31 01:58:57 +0800

    up

--- scripts/data/PyDatas/trigger_config_Table.py
+++ scripts/data/PyDatas/trigger_config_Table.py
@@ -1,7 +1,7 @@
 ﻿# -*- coding utf-8 -* -
 
 datas={
-1001:{ "id":1001, "spaceUID":1, "type":"GateWay", "pos":[450.0,0.2,450.0], "triggerConfig":[[2],[40.0,23.0,20.0]]},
+1001:{ "id":1001, "spaceUID":1, "type":"GateWay", "pos":[450.0,0.2,450.0], "triggerConfig":[[2],[40.0,22.0,20.0]]},
 1002:{ "id":1002, "spaceUID":2, "type":"GateWay", "pos":[20.0,22.0,20.0], "triggerConfig":[[1],[440.0,0.0,440.0]]}
 }
 
