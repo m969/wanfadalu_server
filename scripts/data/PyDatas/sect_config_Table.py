c4dc2e873b2364399c401fa9cab6b9f1ecbcc817
m969 <969041327@qq.com>
2018-05-15 18:40:29 +0800

    up

--- scripts/data/PyDatas/sect_config_Table.py
+++ scripts/data/PyDatas/sect_config_Table.py
@@ -1,7 +1,7 @@
 ﻿# -*- coding utf-8 -* -
 
 datas={
-2:{ "id":2, "spaceUID":2, "sectName":"yun_ling_zong", "pos":[174.0,22.0,137.0], "gatePos":[90.0,22.0,90.0], "gateDir":[0.0,45.0,0.0]}
+2:{ "id":2, "spaceUID":2, "sectName":"yun_ling_zong", "pos":[174.0,22.0,137.0], "gatePos":[90.0,22.0,90.0], "gateDir":[0.0,45.0,0.0], "description":"云灵宗"}
 }
 
 allDatas = {
