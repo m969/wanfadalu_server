03ca26f19376941b6b86047b2922094f1c744678
m969 <969041327@qq.com>
2018-06-03 13:59:00 +0800

    up

---
+++ scripts/data/PyDatas/space_1_spawn_Table.py
@@ -0,0 +1,15 @@
+﻿# -*- coding utf-8 -* -
+
+datas={
+1:{ "id":1, "entity_type":"Monster", "typeID":1001, "spawnPos":[371.0,0.0,235.0]},
+2:{ "id":2, "entity_type":"Monster", "typeID":1002, "spawnPos":[70.0,22.0,60.0]}
+}
+
+allDatas = {
+    "场景1实体出生表":datas,
+}
+
+
+
+
+
